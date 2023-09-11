41cafd1a7214ba23724ce35758f6ea007d18d919
Federico Gonzalez <34560661+fabian3117@users.noreply.github.com>
2023-09-10 21:14:36 -0300

    Update Generalidades.java

--- src/main/java/com/example/demo/utils/Generalidades.java
+++ src/main/java/com/example/demo/utils/Generalidades.java
@@ -5,4 +5,12 @@ public class Generalidades {
     public static final String subjectMailsSendSummary="Resumenes";
 
     public static final String toMailsTest="fabian3117@frba.utn.edu.ar";
+    public static final String Direccion="C:\\home\\site\\wwwroot\\";
+    public static final String Separador="\\";
+    public static String MensajeMailApuntes="Tus apuntes";
+    public static final int anioDefault=1;
+    public static final int cuatrimestreDefault=1;
+    public static final String MensajeWhatssappDefault="Hola," +
+            "Gracias por usar nuestro servicio :D" +
+            "Buena cursada";
 }
